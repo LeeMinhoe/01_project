fb0e0593d61317f98f3ee7edd0982ded25404bdc
MINE <minhoe1122@nate.com>
2017-03-17 17:25:32 +0900

    modification ReadJson

--- input.py
+++ input.py
@@ -1,38 +1,48 @@
+#
+#
+#
+#
+
+
+
 import json
 
 
-
+# Packet의 해더 관련 부분 Class
 class Packet_Header:
+	# 초기화
 	def __init__(self, dst_ip, dst_port, protocol):
 		self.dst_ip = dst_ip
-		self.dst_port = dst_port
+		self.dst_port = int(dst_port)
 		self.protocol = protocol
-
+	# Packet 해더 부분 출력문
 	def print_Header_info(self):
 		print("IP address : ", self.dst_ip)
 		print("port address : ", self.dst_port)
 		print("protocol : ", self.protocol)
 
 
-
+# Packet의 데이터 관련 부분 Class
 class Packet_Data:
+	# 초기화
 	def __init__(self, isRandom, pps, json_file_name):
 		self.isRandom = isRandom
 		self.pps = pps
 		self.json_file_name = json_file_name
-	
+	# Packet 데이터 부분 출력문
 	def print_Data_info(self):
 		print("is Random : ", self.isRandom)
 		print("pps : ", self.pps)
 		print("json_file_name : ", self.json_file_name)
 
 
-
+# Packet Class : Header + Data
 class Packet:
+	# 초기화
 	def __init__(self, Header_part, Data_part):
 		self.Header_part = Header_part
 		self.Data_part = Data_part
-
+	# Packet 정보 출력문
 	def print_packet_info(self):
 		self.Header_part.print_Header_info()
 		self.Data_part.print_Data_info()
@@ -40,7 +50,8 @@ class Packet:
 
 
 
-
+# 입력 인자 받는 함수
+# 예외처리 필요 : 각 입력(json 파일 이름 제외)에 대한 범위 설정 하여 예외처리
 def inputModule():
 	dst_ip = input("Destination IP address : ")
 	dst_port = input("Destination Port address : ")
@@ -56,19 +67,11 @@ def inputModule():
 	return packet
 
 
-def ReadJson(json_file_name):
-	#print(json_file_name)
-	jsonFile = json_file_name
-	with open(jsonFile) as data_file:
-		DataStructure = json.load(data_file)
-
-	print(DataStructure)
-
-	return DataStructure
 
 
 
-	
+
+# 입력 모듈의 전체적인 메인 함수
 def run():
 	packet = inputModule()
 
