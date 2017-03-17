6a221f375058c960609cef46b4823909f2cbf27f
MINE <minhoe1122@nate.com>
2017-03-17 22:47:11 +0900

    add rand module

--- send_packet.py
+++ send_packet.py
@@ -1,17 +1,40 @@
 import socket
 import json
 import pickle
+import string
+import random
 
+def str_generator(size, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
 
-def ReadJson(json_file_name):
-	#print(json_file_name)
-	jsonFile = json_file_name
+def rand_json(JS):
+	
+	JSon = JS
+	t1 = 1073741823
+	t2 = 1073741823 * (-1)
+	Size = len(JSon["Data"])
+
+	for i in range(0,Size):
+		if( JSon["Data"][i]["Type"] == "int"):
+			JSon["Data"][i]["value"] = random.randint(t2,t1)
+		elif( JSon["Data"][i]["Type"] == "float"):
+			JSon["Data"][i]["value"] = random.uniform(t2,t1)
+		elif( JSon["Data"][i]["Type"] == "str"):
+			JSon["Data"][i]["value"] = str_generator(len(JSon["Data"][i]["value"]))
+
+	return JSon
+
+def ReadJson(Pkt):
+	#print(Pkt.Data_part.json_file_name)
+	jsonFile = Pkt.Data_part.json_file_name
 	with open(jsonFile) as data_file:
 		DataStructure = json.load(data_file)
 
+	if Pkt.Data_part.isRandom == 'yes':
+		DataStructure = rand_json(DataStructure)
 	
-	#print(DataStructure)
-	#print()
+	print(DataStructure)
+	print()
 
 	return DataStructure
 
@@ -20,7 +43,7 @@ def ReadJson(json_file_name):
 def send_packet_TCP_pk(Pkt):
 	#DataStructure
 	#DS is json format
-	DS = ReadJson(Pkt.Data_part.json_file_name)
+	DS = ReadJson(Pkt)
 
 	#message is pickle format
 	message = pickle.dumps(DS)
@@ -33,8 +56,9 @@ def send_packet_TCP_pk(Pkt):
 	PORT=Pkt.Header_part.dst_port
 	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((HOST,PORT))
-
-	s.send(message)
+	
+	for i in range(0, Pkt.Data_part.pps):
+		s.send(message)
 	#data=s.recv(1024)
 
 	s.close()
@@ -47,15 +71,16 @@ def send_packet_TCP_pk(Pkt):
 def send_packet_UDP_pk(Pkt):
 	#DataStructure
 	#DS is json format
-	DS = ReadJson(Pkt.Data_part.json_file_name)
+	DS = ReadJson(Pkt)
 
 	message = pickle.dumps(DS)
 
 	HOST=Pkt.Header_part.dst_ip
 	PORT=Pkt.Header_part.dst_port
 	s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-	s.sendto(message, (HOST, PORT))
+	
+	for i in range(0, Pkt.Data_part.pps):
+		s.sendto(message, (HOST, PORT))
 	#data, addr=s.recvfrom(1024)  
 	#print(data)
 
