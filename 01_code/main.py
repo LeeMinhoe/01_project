e4522c0caf7f5e27ab251c1b25de0a04a03bffee
MINE <minhoe1122@nate.com>
2017-03-28 09:18:21 +0900

    ADD single / multi / *.json / directory transport module

--- 01_code/main.py
+++ 01_code/main.py
@@ -1,20 +1,57 @@
+
 from header import *
 
 def run():
 
-	packet = inputModule(argv[1])
+	Pkt = []
+	optionNum = len(sys.argv) - 1
+	
+	# input is '*.json files in ./99_JSON Directory
+	if optionNum == 1 and argv[1] == '*.json' :
+		optionNum = 0
+		fileList = []
+		for file in os.listdir(os.getcwd() + '/../99_JSON'):
+			if file.endswith(".json"):
+				fileList.append(file)
+				optionNum = optionNum + 1
 
-	print()
-
-	packet.print_packet_info()
-
-	print()
+		#print(fileList)
+		for i in range(0, optionNum):
+			Pkt.append(inputModule(fileList[i]))
 
 	
-	if packet.Header_part.protocol == 'TCP':
-		send_packet_TCP_pk(packet)
-	elif packet.Header_part.protocol == 'UDP':
-		send_packet_UDP_pk(packet)
+	# input is Directory name 
+	elif optionNum == 1 and os.path.isdir(os.getcwd() + '/../99_JSON/' + argv[1]):
+		optionNum = 0
+		fileList = []
+		for file in os.listdir(os.getcwd() + '/../99_JSON/' + argv[1]):
+			if file.endswith(".json"):
+				fileList.append(file)
+				optionNum = optionNum + 1
+		for i in range(0, optionNum):
+			Pkt.append(inputModule(fileList[i]))
+
+
+	# input is single transport / multi transport
+	else : 	
+		for i in range(1, optionNum + 1):
+			Pkt.append(inputModule(argv[i]))
+
+	print(Pkt)
+	
+	print()
+	
+	# print Info / Send Packet
+	for i in range(0, optionNum):
+		Pkt[i].print_packet_info()
+		
+		print()
+
+		if Pkt[i].Header_part.protocol == 'TCP':
+			send_packet_TCP_pk(Pkt[i])
+		elif Pkt[i].Header_part.protocol == 'UDP':
+			send_packet_UDP_pk(Pkt[i])
+
 
 if __name__ == "__main__":
 	run()
