ff0064aaa4182a610a6a04fa7abdf0aac05bb9eb
MINE <minhoe1122@nate.com>
2017-04-05 12:33:11 +0900

    log module modify

--- 01_code/log.py
+++ 01_code/log.py
@@ -11,17 +11,19 @@ def makelog(Pkt):
 	filepath = logDirPWD + filename
 	nowDate = now.strftime('%Y-%m-%d')
 	
+	print(os.getcwd())
+
 	os.chdir(logDirPWD)
+	
 	
 	f = open(filename, 'a')
 	
 	f.write("[ Send Time is " + nowDate + " " + nowTime + " ]\n(DST IP : PORT / PROTOCOL)\n")
 	f.write(str(Pkt.Header_part.dst_ip) + " : " + str(Pkt.Header_part.dst_port) + " / " + str(Pkt.Header_part.protocol))
 	f.write("\n===== Detail Data =====\n")
-	print(Pkt.Data_part.DataField)
-	print(len(Pkt.Data_part.DataField))
 	for i in range(len(Pkt.Data_part.DataField)):
-		f.write(str(Pkt.Data_part.DataField[i]["value"]) + "( " + str(Pkt.Data_part.DataField[i]["Type"]) + " )\n")
+		for j in range(len(Pkt.Data_part.DataField[i])):
+			f.write(str(Pkt.Data_part.DataField[i][j]["value"]) + "( " + str(Pkt.Data_part.DataField[i][j]["Type"]) + " )\n")
 	f.write("=======================\n")
 	f.write('\n\n')
 
