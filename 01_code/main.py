79a663c7593403e5ff20c7d0d0b71185d4a208e5
MINE <minhoe1122@nate.com>
2017-04-12 13:21:15 +0900

    add pulse time function

--- 01_code/main.py
+++ 01_code/main.py
@@ -2,6 +2,7 @@ from header import *
 from threading import Thread
 import optparse
 
+
 def th_f(pkt):
 	if pkt.Header_part.protocol == 'TCP':
 		send_packet_TCP_pk(pkt)
@@ -11,10 +12,15 @@ def th_f(pkt):
 
 def run():
 	
-	parser = optparse.OptionParser('main.py -f/m/d <target>')
-	parser.add_option("-f", "--file", dest="target_file_name", help="target is json file name", metavar="TARGET")
-	parser.add_option("-m", "--multi", dest="target_mdirectory_name", help="target is name of directory / N session", metavar="TARGET")
-	parser.add_option("-d", "--directory", dest="target_directory_name", help="target is name of directory that has \"header.json\"", metavar="TARGET")
+	parser = optparse.OptionParser('main.py -f/m/d <target> (-p/r/g <option>)')
+	parser.add_option("-f", "--file", dest="target_file_name", help="target is json file name", metavar="TARGET file name")
+	parser.add_option("-m", "--multi", dest="target_mdirectory_name", help="target is name of directory / N session", metavar="TARGET dir name")
+	parser.add_option("-d", "--directory", dest="target_directory_name", help="target is name of directory that has \"header.json\"", metavar="TARGET dir name")
+
+
+	parser.add_option("-p", "--pulse", dest="steady", metavar="sec", type='float', help="input : float - steady pulse time (sec)")
+	parser.add_option("-r", "--random", dest="random", metavar="from to", type='int', nargs=2, help="input : int ,int - random pulse time range from (int) to (int)")
+	parser.add_option("-g", "--gauss", dest="gauss", metavar="avg sd", type='int', nargs = 2, help="input : int, int - Average, Standard Deviation / According to gaussian distributions")
 
 	(options, args) = parser.parse_args()
 	if len(sys.argv)-1 == 0:
@@ -26,6 +32,7 @@ def run():
 	#1 -f : single
 	if options.target_file_name != None:
 		Pkt.append(inputModule(options.target_file_name, "."))
+		print("#1")
 
 	#2 -m : multi
 	elif options.target_mdirectory_name != None:
@@ -33,59 +40,102 @@ def run():
 		for file in os.listdir(os.getcwd() + '/../99_JSON/' + options.target_mdirectory_name):
 			if file.endswith(".json"):
 				fileList.append(file)
-		for f in fileList:		
-			Pkt.append(inputModule(f, options.target_mdirectory_name))
-			os.chdir(os.getcwd() + '/..')
+		for f in fileList:
+			if f != 'header.json':
+				Pkt.append(inputModule(f, options.target_mdirectory_name))
+				os.chdir(os.getcwd() + '/..')
+		print("#2")
 
 	#3 -d : direcotory
 	elif options.target_directory_name != None:
 		Pkt.append(inputModule("header.json", options.target_directory_name))
 		os.chdir(os.getcwd() + '/..')
+		print("#3")
 	
 	# Pkt List is empty	
 	if len(Pkt) == 0:
 		printe("There is no Json file!")
 		printe("Check 99_JSON directory")
 		exit(1)
-	
-	print()
-	print(len(Pkt))
-	print()
-	# Rand to value module
 
+	# numbering pkt
+	totalPN = 0
+
+	# Rand to value module
 	for p in Pkt:
 		veri_type_checker(p.Data_part.DataField)
 		RandToValue(p.Data_part.DataField)
 		Converter(p.Data_part.DataField)
 		veri_range_checker(p.Data_part.DataField)
+		
+		if len(p.Data_part.DataField) != 0:
+			totalPN += p.Data_part.pps * len(p.Data_part.DataField)
+	#
+	sendPKTCount = 0
+	t = []
+	alladder = 0
+	
+
+	if options.gauss != None:
+		m = options.gauss[0]
+		sd = options.gauss[1]
+		for i in range(totalPN) :
+			z = random.gauss(m, sd)
+			if z < 0 : z = (-1) * z
+			t.append(z)
+			alladder += z
+		t.sort()
+	elif options.steady != None:
+		for i in range(totalPN):
+			t.append(options.steady * (i+1))
+			alladder += t[i]
+	elif options.random != None:
+		t.append(random.uniform(options.random[0], options.random[1]))
+		alladder += t[0]
+		for i in range(totalPN-1):
+			t.append(t[i] + random.uniform(options.random[0], options.random[1]))
+			alladder += t[i]
+
+	print(t)
+	print(alladder / totalPN)
 
 
 	# Transmission Packet
 	# print Info / Send Packet
 	th_p = []
-	
+	print("start : " + str(datetime.now()))
 	for i in range(0, len(Pkt)):
 		Pkt[i].print_packet_info()
 
 		print()
 
-		if Pkt[i].Header_part.protocol == 'TCP':
-			#send_packet_TCP_pk(Pkt[i])
-			th = Thread(target=th_f, args=(Pkt[i], ))
-			th.start()
-			th_p.append(th)
+		if options.steady is None and options.random is None and options.gauss is None:
+			if Pkt[i].Header_part.protocol == 'TCP':
+				#send_packet_TCP_pk(Pkt[i])
+				th = Thread(target=th_f, args=(Pkt[i], ))
+				th.start()
+				th_p.append(th)
 
-		elif Pkt[i].Header_part.protocol == 'UDP':
-			#send_packet_UDP_pk(Pkt[i])
-			th = Thread(target=th_f, args=(Pkt[i], ))
-			th.start()
-			th_p.append(th)
+			elif Pkt[i].Header_part.protocol == 'UDP':
+				#send_packet_UDP_pk(Pkt[i])
+				th = Thread(target=th_f, args=(Pkt[i], ))
+				th.start()
+				th_p.append(th)
+			
+		else :
+			if Pkt[i].Header_part.protocol == 'TCP':
+				send_packet_TCP_pulse(Pkt[i], t, sendPKTCount)
+			elif Pkt[i].Header_part.protocol == 'UDP':
+				send_packet_UDP_pulse(Pkt[i], t, sendPKTCount)
 
-	for th in th_p:
-		th.join()
-	
+	if options.steady is None and options.random is None and options.gauss is None:
+		for th in th_p:
+			th.join()
+		
 	for pkt in Pkt:
 		makelog(pkt)
+
+	print("end   : " + str(datetime.now()))
 
 if __name__ == "__main__":
 	run()
